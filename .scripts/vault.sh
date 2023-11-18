e5069b1675bb1a3a62b22e63c68cfb7589f3dc94
Linda Jansson <linjan2@gmail.com>
2023-11-18 23:02:34 +0100

    add comments to .scripts/vault.sh

--- .scripts/vault.sh
+++ .scripts/vault.sh
@@ -1,6 +1,8 @@
 set -o errexit
 set -o nounset
 
+# Create a vault file from a variables YAML file.
+# Encrypted variables are prefixed with 'vault_', and a vault.vars.yml maps the non-prefixed variables to these.
 function encrypt    # USAGE: encrypt vars.yml inventory/host_vars/localhost/ [password.txt]
 {
   VARS_FILE="${1}"
@@ -8,8 +10,10 @@ function encrypt    # USAGE: encrypt vars.yml inventory/host_vars/localhost/ [pa
   PASSWORD_FILE="${3:-}"
   mkdir -p "${VAULT_DIR}" &>/dev/null || :
 
+  # create mappings file containg: VAR: "{{ vault_VAR }}"
   sed -E 's/^(.+):.+$/\1: "{{ vault_\1 }}"/g' "${VARS_FILE}" > "${VAULT_DIR}/vault.vars.yml"
 
+  # generate a variables file with prefixed variables, and then encypt it into a vault file
   sed -E 's/^(.+):(.+)$/vault_\1: \2/g' "${VARS_FILE}" | \
   ansible-vault encrypt \
     --output "${VAULT_DIR}/vault" \
@@ -17,6 +21,8 @@ function encrypt    # USAGE: encrypt vars.yml inventory/host_vars/localhost/ [pa
     /dev/fd/0
 
 }
+
+# decrypt a vault file
 function decrypt    # USAGE: decrypt vault [password.txt]
 {
   VAULT_FILE="${1}"
